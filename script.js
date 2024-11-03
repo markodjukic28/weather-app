f802f5f6d6ac803cced9b67a9303d555766bdaea
markodjukic28 <markogvs@gmail.com>
2024-11-03 21:44:07 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const apiKey = "b2dc8a08d1e9ca58aaa07b2beac859d4";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  if (!city) {
+    console.error("City name is empty");
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+    return;
+  }
+
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    let data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", function () {
+  checkWeather(searchBox.value.trim());
+});
+
+searchBox.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    checkWeather(searchBox.value.trim());
+  }
+});
